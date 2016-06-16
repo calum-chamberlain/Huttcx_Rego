8d6f62d74834a395e91886328eb3c178db1737fa
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2016-06-16 04:52:09 +0000

    Begin building non-members sign-on

--- app/src/main/java/com/huttcross/registration/Sign_on.java
+++ app/src/main/java/com/huttcross/registration/Sign_on.java
@@ -2,12 +2,30 @@ package com.huttcross.registration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class Sign_on extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.huttcross.registration.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_on);
+    }
+
+    /** Called when the user clicks the Member button */
+    public void memberSignOn(View view) {
+        // Do something in response to button press
+        Intent intent = new Intent(this, MemberSignOn.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Non-Member button */
+    public void nonmemberSignOn(View view) {
+        // Do something in response to button press
+        Intent intent = new Intent(this, NonMemberSignOn.class);
+        startActivity(intent);
     }
 }
