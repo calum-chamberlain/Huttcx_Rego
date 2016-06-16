a57254f28e5e7d46a538f99f431cf542e7268309
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2016-06-16 19:35:39 +1200

    Add member options and spinner

--- app/src/main/java/com/huttcross/registration/MemberSignOn.java
+++ app/src/main/java/com/huttcross/registration/MemberSignOn.java
@@ -23,20 +23,11 @@ public class MemberSignOn extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Find me in database", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Spinner spinner = (Spinner) findViewById(R.id.grade);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-        R.array.Race_grades, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
     }
 
 }
